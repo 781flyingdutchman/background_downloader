75fa2cd45461c4c044e253db4fb57aed2d1059be
Pedro Nogueira Prado dos Santos <74007598+pedrosantosdd@users.noreply.github.com>
2024-12-17 10:30:49 -0300

    fix: update code based on recent main branch

--- android/src/main/kotlin/com/bbflight/background_downloader/TaskWorker.kt
+++ android/src/main/kotlin/com/bbflight/background_downloader/TaskWorker.kt
@@ -398,8 +398,6 @@ open class TaskWorker(
             timerForTimeOut?.schedule(taskTimeoutMillis) {
                 isTimedOut =
                     true // triggers .failed in [TransferBytes] method if not runInForeground
-                timerForTimeOut?.cancel()
-                timerForTimeOut?.purge()
             }
             task = Json.decodeFromString(inputData.getString(keyTask)!!)
             notificationConfigJsonString = inputData.getString(keyNotificationConfig)
@@ -535,11 +533,19 @@ open class TaskWorker(
                 )
 
                 is CancellationException -> {
-                    Log.i(
-                        TAG,
-                        "Canceled task with id ${task.taskId}: ${e.message}"
-                    )
-                    return TaskStatus.canceled
+                    if (BDPlugin.canceledTaskIds.contains(task.taskId)) {
+                        Log.i(
+                            TAG,
+                            "Canceled task with id ${task.taskId}: ${e.message}"
+                        )
+                        return TaskStatus.canceled
+                    } else {
+                        Log.i(
+                            TAG,
+                            "WorkManager CancellationException for task with id ${task.taskId} without manual cancellation: failing task"
+                        )
+                        return TaskStatus.failed
+                    }
                 }
 
                 else -> {
